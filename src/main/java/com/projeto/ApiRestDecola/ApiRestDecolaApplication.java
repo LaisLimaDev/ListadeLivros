663285ca997c4ba543f641ca484d45824763b1a9
Lais Lima <laissp25@gmail.com>
2025-03-23 23:56:00 -0300

    Configuração da API

---
+++ src/main/java/com/projeto/ApiRestDecola/ApiRestDecolaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.ApiRestDecola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDecolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDecolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/ApiRestDecola/ApiRestDecolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.ApiRestDecola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDecolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
